773c51f8fe81a97ef5e52763bc4671ee612e22fc
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-24 11:23:26 -0400

    Land Cost Calculator
    Added code for problem 2 to calculate the cost of land based on costs
    per sq yard and per foot of perimeter.

--- NumbersAndStrings/Program.cs
+++ NumbersAndStrings/Program.cs
@@ -12,6 +12,7 @@ namespace NumbersAndStrings
         {
             // Accumulation Problem
 
+            /*
             decimal deposit = 35.00M;
 
             Console.WriteLine("If you save " + deposit + " every day, you will have:");
@@ -19,6 +20,27 @@ namespace NumbersAndStrings
             Console.WriteLine((deposit * 365 * 2) + " after 2 years.");
             Console.WriteLine((deposit * 365 * 5) + " after 5 years.");
             Console.WriteLine((deposit * 365 * 10) + " after 10 years.");
+
+            Console.ReadLine();
+            */
+            
+            // Land Cost Calculator
+
+            decimal sqYardCost = 5.0M;
+            decimal perimeterCost = 0.75M;
+            int length;
+            int width;
+
+            Console.WriteLine("Enter the length & width of the land parcel you are buying:");
+            Console.Write("Length: ");
+            length = int.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            decimal landcost = (length * width * sqYardCost) + (((length * 2) + (width * 2)) * perimeterCost);
+            Console.WriteLine("The cost of the land you're buying is {0:C}", landcost);
 
             Console.ReadLine();
 
